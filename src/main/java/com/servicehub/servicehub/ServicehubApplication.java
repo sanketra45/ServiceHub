d8f73b284d54a6739e7f9ea5ffdf0ee66ef0aa0d
Sanket Rathod <sanketra53@gmail.com>
2026-03-19 14:52:03 +0530

    Initial commit

---
+++ src/main/java/com/servicehub/servicehub/ServicehubApplication.java
@@ -0,0 +1,13 @@
+package com.servicehub.servicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicehub/servicehub/ServicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicehub.servicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
