1d4250fbacca69ba591c0d155a7e33dc3d5e28fa
Sanket Rathod <sanketra53@gmail.com>
2026-04-04 01:56:38 +0530

    FRONTEND READY TO DEPLOY

--- servicehub-ui/src/api/axios.js
+++ servicehub-ui/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://servicehub-r2bh.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
